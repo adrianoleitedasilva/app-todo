bfe94b827c0b920ac0f228e9b571ddb202c19db0
Adriano Leite da Silva <adrianoleitedasilva@outlook.com>
2023-09-03 22:06:00 -0300

    update: inserindo a funcionalidade de insercao das tasks

--- src/app/modules/home/components/todo-input-add-items/todo-input-add-items.component.ts
+++ src/app/modules/home/components/todo-input-add-items/todo-input-add-items.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-items',
   templateUrl: './todo-input-add-items.component.html',
   styleUrls: ['./todo-input-add-items.component.scss']
 })
+
 export class TodoInputAddItemsComponent {
+  @Output() public emitItemTastList = new EventEmitter();
+  public addItemTaskList: string = "";
+
+  public submitItemTaskList() {
+    if(this.addItemTaskList){
+      this.emitItemTastList.emit(this.addItemTaskList);
+      this.addItemTaskList = "";
+    }
+  }
 
 }
--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -10,12 +10,20 @@ import { TaskList } from '../../model/task-list';
 })
 
 export class TodoListComponent  {
-  public taskList: Array<TaskList> = [
-    { task: "Minha nova task", checked: true },
-    { task: "Minha nova task 2", checked: false },
-  ];
+  public taskList: Array<TaskList> = [];
+
+  public setEmitTaskList(event: string) {
+    console.log(event);
+    this.taskList.push({ task: event, checked: false });
+  }
 
   public deleteItemTaskList(event: number) {
     this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList() {
+    const confirm = window.confirm("Deseja realmente apagar tudo?");
+    if (confirm) { this.taskList = []; }
+    
   }
 }
